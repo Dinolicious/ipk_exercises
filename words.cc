30d01209ed5fdcd362c12f3ea4ca72aafa548c59
Tim Gaisbauer <Gaisbauer@stud.uni-heidelberg.de>
2017-12-17 13:31:51 +0100

    words almost done

--- words.cc
+++ words.cc
@@ -2,39 +2,38 @@
 #include<map>
 #include<cctype>
 #include<string>
-#include"sanitizedword.hh"
+#include"sanitizeword.hh"
 
-std::map<char,int> get_frequencies(){
-    std::map<char,int> output;
-    std::cout<<"Start writing words!"<<std::endl;
-    while (true){
+std::map<std::string, int> get_frequencies(){
+    std::map<std::string, int> output;
+    std::cout << "Start writing words:" << std::endl;
+    while(true){
         std::string s;
         std::cin >> s;
         if (!std::cin)
             break;
         sanitize_word(s);
-            if (s.size() !=0)
-                output[s]++
-
+        if (s.size() != 0)
+            output[s]++;
     }
     return output;
 }
 
-void print_frequencies(const std::map<char, int>& frequencies){
+void print_frequencies(const std::map<std::string, int>& frequencies){
     int size = 0;
-    for (auto& e : frequencies){
+    for (auto e : frequencies){
         size += e.second;
     }
-    for (auto& e : frequencies){
+    for (auto e : frequencies){
         std::cout << e.first << ": " << (double)e.second / (double)size << std::endl;
     }
-    std::cout << "Total words: " << size;
+    std::cout << "Total words: " << size << std::endl;
 }
 
-std::map<std::string,int> get_frequencies();
+std::map<std::string, int> get_frequencies();
 void print_frequencies(const std::map<std::string, int>&);
 
-int main{
+int main(){
     print_frequencies(get_frequencies());
 
     return 0;
