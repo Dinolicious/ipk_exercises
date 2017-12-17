84447375d6bd97b2b65225032afadfc69d598968
Tim Gaisbauer <Gaisbauer@stud.uni-heidelberg.de>
2017-12-17 13:11:36 +0100

    letters.cc finished, txt added

--- letters.cc
+++ letters.cc
@@ -7,26 +7,44 @@ std::map<char,int> get_frequencies(){
     std::map<char,int> outmap;
     std::cout<<"Start writing letters!"<<std::endl;
     while (true){
-        unsigned char c;                        // read in character
+        unsigned char c;                                                                    //read in character
         std::cin >> c;
-        if (!std::cin)                        // abort if input closed
-        break;
-    outmap[c]++;
+        if (!std::cin)                                                                      //abort if input closed
+            break;
+        if (isalpha(c))                                                                     //check if letter or sign
+            outmap[toupper(c)]++;
     }
     return outmap;
 }
 
-void print_frequencies(const std::map<char,int>& frequencies){
-    for (auto entry : frequencies){
-        std::cout<<entry.first<<":";
-        std::cout<<entry.second<<std::endl;
+void print_frequencies(const std::map<char, int>& frequencies){                             //giving number of signs
+    int size = 0;
+    for (auto& e : frequencies){                                                            //total number of letters
+        size += e.second;
     }
+    for (auto& e : frequencies){
+        std::cout << e.first << ": " << (double)e.second / (double)size << std::endl;       //probability of letters
+    }
+    std::cout << "Total letters: " << size;
 }
+
+bool isalpha(char c){
+    return std::isalpha(static_cast<unsigned char>(c));                                     //counts only letters, no signs
+}
+
+char toupper(char c){                                                                       //small and big letters into one category
+    return std::toupper(static_cast<unsigned char>(c));
+}
+
 
 
 int main(){
     
     print_frequencies(get_frequencies());
+
+    bool isalpha(char);
+
+    char toupper(char);
 
     return 0;
 }
