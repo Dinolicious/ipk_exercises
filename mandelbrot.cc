59200bc21fe1aa510e7d55a88fd8af4e46fd2f6c
Tim Gaisbauer <Gaisbauer@stud.uni-heidelberg.de>
2017-12-18 12:30:08 +0100

    mandelbrot anfang

---
+++ mandelbrot.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include"canvas.hh"
+
+void mandelbrot (Canvas& canvas, double threshold,int maxIt, std::string filename)
+
+class IterationResult{
+    IterationResult();
+    point(double c1=0, double c2=0);
+    Iteration(int i=0);
+
+}
+
+
+int main{
+    return 0;
+}
