54faf64014d456ca9384f2263a79d2fe771c2813
Tim Gaisbauer <Gaisbauer@stud.uni-heidelberg.de>
2018-01-05 13:39:40 +0100

    mandelbrot+iterationresult

--- iterationresult.hh
+++ iterationresult.hh
@@ -9,11 +9,25 @@ class IterationResult{                                                          
     int _counter;
 
     public:
-        IterationResult(lastPoint): _lastPoint(lastPoint), _counter(0) {}
-
-        Point lastPoint(0) const{
-            return lastPoint;
+        Point lastPoint(0) const{                                                           //
+            return _lastPoint;
         }
-        IterationResult iteration(Point, Point, double, int);
-};
+        int counter() const{
+            return _counter;
+        }
+        void iterate( Point, Point, double, int);
+}
 
+void IterationResult::iterate(Point z, Point c, double threshold, int maxIt){
+    _lastPoint = z;
+    _counter = 0;
+    
+    while (_lastPoint.length() <= threshold && _counter <= maxIt){
+        _lastPoint.x(pow(_lastPoint.x(), 2.) + pow(_lastPoint.y(), 2) + c.x());
+        _lastPoint.y(2 * _lastPoint.x() * _lastPoint.y() + c.y());
+
+        _counter++;
+    }
+}
+
+#endif
