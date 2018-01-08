d26da79ed2d689e0787bf90ceb4bd595e5ad98e1
Tim Gaisbauer <Gaisbauer@stud.uni-heidelberg.de>
2018-01-08 12:11:00 +0100

    Mandelbrot anfang

--- iterationresult.hh
+++ iterationresult.hh
@@ -5,21 +5,21 @@
 #include"Point.hh"
 
 struct IterationResult{
-    Point lastPoint;
+    Point oldPoint;
     int counter;
 
-    IterationResult(Point point, int newcounter): lastPoint(point), counter(newcounter) {}
+    IterationResult(Point point, int newcounter): oldPoint(point), counter(newcounter) {}
 }
 
 void IterationResult::iterate(Point z, Point c, double threshold, int maxIt){
     IterationResult output(z,0);
     
-    while (distance(z, output.lastPoint) < threshold && output.counter < maxIt){
-        Point lastPoint = output.lastPoint;
+    while (distance(z, output.oldPoint) < threshold && output.counter < maxIt){
+        Point oldPoint = output.oldPoint;
 
-        lastPoint.x(pow(lastPoint.x(), 2) - pow(lastPoint.y(), 2) + c.x(), 2* lastPoint.y(2 * lastPoint.x() * lastPoint.y() + c.y());
+        oldPoint.x(pow(oldPoint.x(), 2) - pow(oldPoint.y(), 2) + c.x(), 2* oldPoint.y(2 * oldPoint.x() * oldPoint.y() + c.y());
 
-        output.lastPoint = newPoint;
+        output.oldPoint = newPoint;
         output.counter++;
     }
     return output;
---
+++ mandelbrot.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include "canvas.hh"
+#include "iteration.hh"
+
+void mandelbrot(Canvas& canvas, double threshold, int maxIt, std::string filename);
+
