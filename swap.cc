65fee7860367b94bccaf0b1400a910d0b89a6ca1
Dinolicious <34235505+Dinolicious@users.noreply.github.com>
2018-01-21 23:48:43 +0100

    Add files via upload

---
+++ swap.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template<typename T>
+void swap (T&, T&);
+
+int main(){
+    int a = 1;
+    int b = 2;
+    std::cout << "a = " << a << " b = " << b << std::endl;
+    
+    swap(a, b);
+    std::cout << "a = " << a << " b = " << b << std::endl;
+    
+    double c = 3;
+    double d = 4;
+    std::cout << "c = " << c << " d = " << d << std::endl;
+    
+    swap(c, d);
+    std::cout << "c = " << c << " d = " << d << std::endl;
+
+    return 0;
+}
+
+template<typename T>
+void swap (T& a, T& b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
